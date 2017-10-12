43f537d4695a9de380f8c00efb6aeeb491b12a6f
Matt Hough <matt@hough.tv>
2017-10-12 12:25:27 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,9 @@
-class appointment
+class Appointment
   attr_reader :date, :doctor
 
   def initialize(date, doctor)
     @date = date
     @doctor = doctor
-    doctor.add_appointment(self) = self
+    doctor.add_appointment(self)
   end
 end
